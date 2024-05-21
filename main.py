aa7c5a0573ce7e73a3fdcfe83832b285e2af8ed6
Cybersport <Cybersport@26kadr.internal>
2024-05-21 13:49:36 +0300

    second commit

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from aiogram import Bot, Dispatcher, types, executor
 from config import TELEGRAM_TOKEN
+from keboard.keyboards import get_keyboard_1, get_keyboard_2
 
 bot = Bot(token= TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
+
+
 
 async def set_commands(bot: Bot):
     commands = [
@@ -13,10 +16,28 @@ async def set_commands(bot: Bot):
 
 
 
-
 @dp.message_handler(commands= 'start')
 async def start(message: types.Message):
-    await message.answer('Здраствуйте, я вертуальный бот :) ')
+    await message.answer('Здраствуйте, я вертуальный бот :) ', reply_markup= get_keyboard_1())
+
+@dp.message_handler(lambda massage: massage.text == 'Отправь фото DragonBall')
+async def button_1_click(message: types.Message):
+    await bot.send_photo(message.chat.id, photo= 'https://seeklogo.com/images/G/goku-symbol-logo-C3157507EA-seeklogo.com.png?v=638372782130000000', caption= "Dragon Ball!")
+
+@dp.message_handler(lambda massage: massage.text == 'Перейти на след.клавиатуру')
+async def button_2_click(message: types.Message):
+    await message.answer('Тут ты можешь попросить отправить фото BlackClover', reply_markup= get_keyboard_2())
+
+
+
+@dp.message_handler(lambda massage: massage.text == 'Отправь фото BlackClover')
+async def button_3_click(message: types.Message):
+    await bot.send_photo(message.chat.id, photo= 'https://logowik.com/content/uploads/images/black-clover8928.jpg', caption= "Black Clover!")
+
+@dp.message_handler(lambda massage: massage.text == 'Перейти на след.клавиатуру')
+async def button_2_click(message: types.Message):
+    await message.answer('Тут ты можешь попросить отправить фото DragonBall', reply_markup= get_keyboard_1())
+
 
 @dp.message_handler(commands= 'help')
 async def help(message: types.Message):
