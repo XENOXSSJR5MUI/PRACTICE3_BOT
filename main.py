7e1415c3818c8a40c5566476a00733946773f0fa
Cybersport <Cybersport@26kadr.internal>
2024-05-23 12:55:27 +0300

    готовая инлайн клавиатура

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from aiogram import Bot, Dispatcher, types, executor
 from config import TELEGRAM_TOKEN
 from keboard.keyboards import get_keyboard_1, get_keyboard_2
+from keboard.key_inline import get_keyboard_inline
 
 bot = Bot(token= TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
+
 
 
 
@@ -20,28 +22,27 @@ async def set_commands(bot: Bot):
 async def start(message: types.Message):
     await message.answer('Здраствуйте, я вертуальный бот :) ', reply_markup= get_keyboard_1())
 
-@dp.message_handler(lambda massage: massage.text == 'Отправь фото DragonBall')
+@dp.message_handler(lambda message: message.text == 'Отправь фото DragonBall')
 async def button_1_click(message: types.Message):
-    await bot.send_photo(message.chat.id, photo= 'https://seeklogo.com/images/G/goku-symbol-logo-C3157507EA-seeklogo.com.png?v=638372782130000000', caption= "Dragon Ball!")
+    await bot.send_photo(message.chat.id, photo= 'https://seeklogo.com/images/G/goku-symbol-logo-C3157507EA-seeklogo.com.png?v=638372782130000000', caption= "Dragon Ball!", reply_markup= get_keyboard_inline())
 
-@dp.message_handler(lambda massage: massage.text == 'Перейти на след.клавиатуру')
+@dp.message_handler(lambda message: message.text == 'Перейти на след.клавиатуру')
 async def button_2_click(message: types.Message):
     await message.answer('Тут ты можешь попросить отправить фото BlackClover', reply_markup= get_keyboard_2())
 
 
 
-@dp.message_handler(lambda massage: massage.text == 'Отправь фото BlackClover')
+@dp.message_handler(lambda message: message.text == 'Отправь фото BlackClover')
 async def button_3_click(message: types.Message):
     await bot.send_photo(message.chat.id, photo= 'https://logowik.com/content/uploads/images/black-clover8928.jpg', caption= "Black Clover!")
 
-@dp.message_handler(lambda massage: massage.text == 'Перейти на след.клавиатуру')
-async def button_2_click(message: types.Message):
-    await message.answer('Тут ты можешь попросить отправить фото DragonBall', reply_markup= get_keyboard_1())
-
+@dp.message_handler(lambda message: message.text == 'Вернуться на 1 клавиатуру')
+async def button_4_click(message: types.Message):
+    await message.answer('Тут ты можешь попросить отправить фото BlackClover', reply_markup= get_keyboard_1())
 
 @dp.message_handler(commands= 'help')
 async def help(message: types.Message):
-    await message.reply('Чем вам помочь? ')
+    await message.reply('Чем вам помочь?')
 
 @dp.message_handler()
 async def echo(message: types.Message):
